14f21684f6ed49c1a80aa4362bd9f0e5b60eea1f
kondoryu <13jk104@ms.dendai.ac.jp>
2015-12-19 07:59:57 +0900

    konsatur

--- Assets/BattleCamera.cs
+++ Assets/BattleCamera.cs
@@ -13,7 +13,7 @@ public class BattleCamera : MonoBehaviour {
 		myTimer += Time.deltaTime;
 		if(myTimer >=8){{
 				transform.position = Vector3.MoveTowards (transform.position , 
-			     new Vector3( -364.8f, 135f, -147.3f) , 1.0f);   //絶対座標の(10,10,10)に向かって0.1移動させる
+			     new Vector3( 4.01f, 2.48f, -1.52f) , 0.4f);   //絶対座標の(10,10,10)に向かって0.1移動させる
 	}
 }
 	}
