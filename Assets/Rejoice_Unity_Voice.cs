6cb117f88cdb04a6b7430e0a52ca36eef6b36d2b
ishikawa0405 <13jk017@ms.dendai.ac.jp>
2016-03-07 12:44:59 +0900

    by ryu

---
+++ Assets/Rejoice_Unity_Voice.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rejoice_Unity_Voice : MonoBehaviour {
+	
+	public AudioSource sound01;
+	float myTimer = 0.0f;
+	bool hantei = false;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		myTimer += Time.deltaTime;
+		if(myTimer > 0.5) {
+			if(hantei == false){
+				sound01.PlayOneShot(sound01.clip);
+				hantei = true;
+			}
+		}
+	}
+}
---
+++ Assets/Unity_Omedetou_Voice.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unity_Omedetou_Voice : MonoBehaviour {
+	
+	public AudioSource sound01;
+	float myTimer = 0.0f;
+	bool hantei = false;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		myTimer += Time.deltaTime;
+		if(myTimer > 4.0) {
+			if(hantei == false){
+				sound01.PlayOneShot(sound01.clip);
+				hantei = true;
+			}
+		}
+	}
+}
