c338968bd7d5ae30d414c42f1d7c6fbb2c2a37be
ishikawa0405 <13jk017@ms.dendai.ac.jp>
2016-09-26 18:00:13 +0900

    ryu commit

---
+++ Assets/Open_the_Door.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Open_the_Door : MonoBehaviour {
+	public GameObject lefthand;
+	public GameObject righthand;
+	bool hantei = false;
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (hantei);
+		if (hantei == true) {
+			animator.Play ("Open");
+		}
+	}
+
+	public void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player") {
+			hantei = true;
+		}
+	}
+
+}
