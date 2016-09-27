aa5d78c5dc2c861604d08abe912f580fd684cf3f
NishioTakashi <taka_1108n@i.softbank.jp>
2016-09-27 18:02:05 +0900

    nishiodie
    ohhhhhhhhhhhhh

---
+++ Assets/Nmovecamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nmovecamera : MonoBehaviour {
+
+		Camera mainCamera;//メインカメラ
+		Camera subCamera;//サブのカメラです
+		float myTimer = 0.0f;
+		// Use this for initialization
+		void Start () {
+			mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+			subCamera = GameObject.Find ("Sub Camera").GetComponent<Camera> ();
+			
+			subCamera.enabled = false;
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			// Spaceキーで切り替え
+			/*if (Input.GetKeyDown(KeyCode.Space))
+  {
+   if(mainCamera.enabled){
+    mainCamera.enabled = false;
+    subCamera.enabled = true;
+   }else{
+    mainCamera.enabled = true;
+    subCamera.enabled = false;
+   }
+  }*/ 
+			myTimer += Time.deltaTime;
+			if(myTimer >=10
+		   ){
+				mainCamera.enabled = false;
+				subCamera.enabled = true;
+			}else{
+				mainCamera.enabled = true;
+				subCamera.enabled = false;
+			}
+		}
+	}
