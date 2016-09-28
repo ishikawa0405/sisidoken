699e5dfdfd5e52b2993dbb41b521fe3100be45ba
kondoryu <13jk104@ms.dendai.ac.jp>
2016-09-28 11:04:01 +0900

    kon.Change

---
+++ Assets/GoodNight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GoodNight : MonoBehaviour {
+	public GameObject GoodNight_bed;
+	float myTimer = 0.0f;
+	bool hantei = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myTimer += Time.deltaTime;
+		if(myTimer >=7.5 && hantei == false){
+			Instantiate(GoodNight_bed, new Vector3(274.8f, 29.8f, 77.7f), Quaternion.Euler (0, -90, 0));
+			hantei = true;
+		}	
+	}
+}
---
+++ Assets/NewBehaviourScript3.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NewBehaviourScript3 : MonoBehaviour {
+	public GameObject BathroomWord;
+	float myTimer = 0.0f;
+	bool hantei = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myTimer += Time.deltaTime;
+		if(myTimer >=0.5 && hantei == false){
+			Instantiate(BathroomWord, new Vector3(63.2f, 10f, 77f), Quaternion.Euler (0, -90, 0));
+			hantei = true;
+		}
+			
+	}
+}
